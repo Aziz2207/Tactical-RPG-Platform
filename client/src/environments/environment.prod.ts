213389fd481075d34161bacb5f22cc69726f2274
Aziz2207 <133813323+Aziz2207@users.noreply.github.com>
2025-01-16 15:39:44 -0500

    Delete client/src/environments/environment.prod.ts

--- client/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    serverUrl: 'http://ec2-3-96-64-27.ca-central-1.compute.amazonaws.com:3000/api', // TODO: Add remote server URL
-    serverGatewayUrl: 'http://ec2-3-96-64-27.ca-central-1.compute.amazonaws.com:3000',
-};
